082611e7d7a3927aee1cd02f50a16c53101774d0
tijmenvangurp <tijmenvangurp@gmail.com>
2014-12-11 23:04:28 +0100

    Only formatting

--- voorbeeld_code_tom/compare_old_new_color_values.ino
+++ voorbeeld_code_tom/compare_old_new_color_values.ino
@@ -1,25 +1,28 @@
 void compare_old_and_new_coulour_setting (byte current_coulour, byte new_collour, byte current_color_place ){
 
   if(current_coulour > new_collour ){
-    if((current_coulour-new_collour) > snelheid){
-    //current_coulour must become smaller     
-    current_collour_setting[row_counter_comp][current_color_place] -= snelheid;
-    
-    }else{
-   current_collour_setting[row_counter_comp][current_color_place] --;
+    if(current_coulour - new_collour > snelheid){
+      //current_coulour must become smaller     
+      current_collour_setting[row_counter_comp][current_color_place] -= snelheid;
+
     }
-    
-  }
-  else if(current_coulour < new_collour){
-    if((new_collour-current_coulour) < snelheid){
-    // current_coulour must become bigger 
-    current_collour_setting[row_counter_comp][current_color_place] += snelheid;
-    
-    }else{
-    current_collour_setting[row_counter_comp][current_color_place]++;
+    else{
+      current_collour_setting[row_counter_comp][current_color_place] --;
+    }
 
   }
- }
+  else if(current_coulour < new_collour){
+    if((new_collour - current_coulour) > snelheid){
+      // current_coulour must become bigger 
+      current_collour_setting[row_counter_comp][current_color_place] += snelheid;
+
+    }
+    else{
+      current_collour_setting[row_counter_comp][current_color_place]++;
+
+    }
+  }
 }
+
 
 

9d5b23bd340586988fba0d7e04678cdccb5d379c
tomvtwesteinde <info@tomvantwesteinde.nl>
2014-12-11 19:54:18 +0100

    Working_without speed
    Oude code + proberen van snelheidsverandering. Krijg echter error.

--- voorbeeld_code_tom/compare_old_new_color_values.ino
+++ voorbeeld_code_tom/compare_old_new_color_values.ino
@@ -1,20 +1,25 @@
 void compare_old_and_new_coulour_setting (byte current_coulour, byte new_collour, byte current_color_place ){
 
   if(current_coulour > new_collour ){
-    if(current_coulour-new_collour > snelheid){
+   // if(current_coulour-new_collour > snelheid){
     // current_coulour must become smaller     
-    current_collour_setting[row_counter_comp][current_color_place] -= snelheid;
+   // current_collour_setting[row_counter_comp][current_color_place] -= snelheid;
     
-    }else{
-    current_collour_setting[row_counter_comp][current_color_place] --;
-    }
+   // }else{
+   current_collour_setting[row_counter_comp][current_color_place] --;
+    //}
     
   }
   else if(current_coulour < new_collour){
+   // if(new_collour-current_coulour < snelheid){
     // current_coulour must become bigerer 
+    //current_collour_setting[row_counter_comp][current_color_place] += snelheid;
+    
+    //}else{
     current_collour_setting[row_counter_comp][current_color_place]++;
 
-  }
-
+  //}
+ }
 }
+
 

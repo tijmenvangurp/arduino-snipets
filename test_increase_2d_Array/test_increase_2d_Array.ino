11f0ccab810ad3f80637d5f6e8ba20da16a1db0c
tijmenvangurp <tijmenvangurp@gmail.com>
2014-12-11 22:17:27 +0100

    just to be sure im not crazy

--- test_increase_2d_Array/test_increase_2d_Array.ino
+++ test_increase_2d_Array/test_increase_2d_Array.ino
@@ -1,11 +1,19 @@
 // Test increase 2D array
 int test [2][2];
 void setup(){
-Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop(){
   test [0][0]--;
   Serial.println(test [0][0]);
+  test [0][0]+= 5;
+  Serial.println(test [0][0]);
+  delay(2000);
+  for(int i = 0; i < 5; i++){
+    Serial.println(i);
+  }
+  delay(2000);
 
 }
+
